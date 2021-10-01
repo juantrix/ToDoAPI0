2f36b39dce97a5139996f7e9ffd6328673930c81
juan <juanostrit66@gmail.com>
2021-09-30 22:27:00 -0300

    inicio

---
+++ main.py
@@ -0,0 +1,68 @@
+import datetime
+from fastapi import FastAPI
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String, Boolean, DateTime, create_engine, select
+from sqlalchemy.orm import sessionmaker
+
+# para conectar la base de datos se le pasa el siguiente formato 'nombreGestor://nombreUser:@direccion/nombreDB'
+engine = create_engine('postgresql://postgres:123456@localhost/pythondb')
+Base = declarative_base()
+app = FastAPI()
+
+class Task(Base):
+    __tablename__ = 'Task'
+
+    id = Column(Integer(), primary_key=True)
+    title = Column(String(32), nullable=False)
+    body = Column(String(128), nullable=False)
+    completed = Column(Boolean, default=False)
+    date = Column(DateTime(), default=datetime.datetime.now())
+
+    def __str__(self) -> str:
+        return self.title
+
+
+Session = sessionmaker(bind=engine)
+session = Session()
+
+@app.post('/new')
+def newTask(title: str, body: str):
+    with Session() as session:
+        session.add(Task(title=title,body=body))
+        session.commit()
+
+    return  'added'
+
+
+@app.get('/tasks')
+def task_list():
+    return session.query(Task).all()
+
+
+@app.delete('/delete')
+def delete_task(id: int):
+    task = session.query(Task).filter(Task.id==id)
+    if task.first() is not None:
+        task.delete()
+        session.commit()
+        return 'Deleted'
+    else:
+        return 'The task not exist'
+
+
+@app.put('/change')
+def change_task(id, title, body, completed):
+    task = session.query(Task).filter(Task.id==id)
+    if task.first() is not None:
+        task.first().body = body
+        task.first().title = title
+        task.first().completed = bool(completed)
+        task.first().date = datetime.datetime.now()
+        return 'Changed'
+    else:
+        return 'The task not exist'
+
+
+if __name__ == '__main__':
+    Base.metadata.drop_all(engine)
+    Base.metadata.create_all(engine)
